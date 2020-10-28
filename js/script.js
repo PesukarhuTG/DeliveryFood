215b481fc50ae43b68a1cb3c0f6b9526b81d5e1c
Tatiana <ganzhinovat@yandex.ru>
2020-10-28 18:33:57 +0300

    Add logic of cards rendering

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 const cartBtn = document.querySelector('#cart-button');
 const modalCart = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close');
@@ -11,13 +12,19 @@ const loginInput = document.querySelector('#login');
 const userName = document.querySelector('.user-name');
 const outBtn = document.querySelector('.button-out');
 
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
+
 let login = localStorage.getItem('delivery');
 
 // open/close modal cart window
 const toggleModal = () => {
     modalCart.classList.toggle('open');
 };
-
 
 //authorization 
 const toggleModalAuth = () => {
@@ -91,10 +98,103 @@ const notAuthorized = () => {
             toggleModalAuth();
         }
     });
-    
 };
+
+//generate cards of restaurants
+const createCardRestaurant = () => {
+
+    const card = `
+    <a class="card card-restaurant">
+        <img class="card-image" src="img/pizza-plus/preview.jpg" alt="card-img">
+        <div class="card-text">
+            <div class="card-heading">
+                <h3 class="card-title">Пицца плюс</h3>
+                <span class="card-tag tag">50 мин.</span>
+            </div>
+            <div class="card-info">
+                <div class="rating">
+                    <img src="./img/star.svg" alt="star">
+                    <span>4.5</span>
+                </div>
+            <div class="price">от 900 ₽</div>
+            <div class="category">Пиццерия</div>
+            </div>
+        </div>
+    </a>
+    `;
+
+    cardsRestaurants.insertAdjacentHTML('beforeend', card);
+
+};
+
+//create card
+const createCardGood = () => {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    card.insertAdjacentHTML('afterbegin', `
+			<img src="img/pizza-plus/pizza-girls.jpg" alt="image" class="card-image"/>
+				<div class="card-text">
+					<div class="card-heading">
+						<h3 class="card-title card-title-reg">Пицца Девичник</h3>
+					</div>
+					<div class="card-info">
+						<div class="ingredients">Соус томатный, постное тесто, нежирный сыр, кукуруза, лук, маслины,
+						грибы, помидоры, болгарский перец.
+						</div>
+					</div>
+					<div class="card-buttons">
+						<button class="button button-primary button-add-cart">
+							<span class="button-card-text">В корзину</span>
+							<span class="button-cart-svg"></span>
+						</button>
+						<strong class="card-price-bold">450 ₽</strong>
+					</div>
+				</div>
+    `);
+
+    cardsMenu.insertAdjacentElement('beforeend', card);
+};
+
+//open menu restaurant after click on card
+const openGoods = (e) => {
+
+    const target = e.target;
+    const restaurant = target.closest('.card-restaurant');
+
+    if (restaurant) {
+
+        //check authorization
+        if (!login) {
+            modalAuth.classList.toggle('open');
+        } else {
+            cardsMenu.textContent = '';
+            containerPromo.classList.add('hide');
+            restaurants.classList.add('hide');
+            menu.classList.remove('hide');
+
+            createCardGood();
+            createCardGood();
+        }
+
+    }
+};
+
+
+
+
+
 
 cartBtn.addEventListener('click', toggleModal);
 closeBtn.addEventListener('click', toggleModal);
+cardsRestaurants.addEventListener('click', openGoods); //клик на карточке через делегирование
+logo.addEventListener('click', () => {
+    containerPromo.classList.remove('hide');
+    restaurants.classList.remove('hide');
+    menu.classList.add('hide');
+});
 
 checkAuth();
+createCardRestaurant();
+createCardRestaurant();
+createCardRestaurant();
